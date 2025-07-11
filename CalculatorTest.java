559c7b096ed40bec0e0dcae8db1fa4e9682ac8fe
SABBANISAIKIRAN <166910781+SABBANISAIKIRAN@users.noreply.github.com>
2025-07-11 11:43:37 +0530

    Delete CalculatorTest.java

--- CalculatorTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.*;
-import org.junit.Test;
-
-public class CalculatorTest {
-    @Test
-    public void testAddition() {
-        assertEquals(4, Calculator.add(2, 2));
-    }
-}
